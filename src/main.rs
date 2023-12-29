577d80cd8808d51832e4352f0c3cc613fff1c67f
KBeKind <klee@LAPTOP-5P7KDISB>
2023-12-29 09:32:12 -0600

    rust basics

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+
+    let a: i32 = 10;
+    let b: i32 = 15;
+    println!("Hello, world! , {} {}", a, b);
+
+
+    let character: char = 'a';
+    println!("Character => {}", character);
+
+    let boolean: bool = true;
+    println!("Boolean => {}", boolean);
+
+    let tuple: (i32, i32, f64, i32, bool) = (1, -2, 3.0, 4, true);
+    println!("Tuple => {:?}", tuple);
+
+
+}
